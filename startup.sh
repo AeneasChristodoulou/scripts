dce8c0d31d43501c700bc72de083d4e64f4b961c
aeneas <streamcloud20@gmail.com>
2022-03-09 17:01:50 +0100

    New Aliases/Shortcuts

--- startup.sh
+++ startup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+
+# New Path
+export PATH="/home/aeneas/.local/bin:$PATH"
+
 # Git Commit Function
 function gitcommit {
 	git add .
@@ -16,4 +20,23 @@ alias cdpythontests="cd /home/aeneas/Projects/pythonTests/"
 # cd scripts
 alias cdscripts="cd /home/aeneas/Projects/Scripts/"
 
- 
+# Update and Upgrade Function
+
+function update {
+	sudo apt-get update
+	sudo apt-get upgrade
+}	
+	
+# source Scripts
+alias sourcescripts="source /home/aeneas/Projects/Scripts/startup.sh"
+
+# cd to Hashcat
+alias cdhashcat="cd /home/aeneas/Projects/hashcat-6.2.5/"
+	
+
+
+# Edit this File
+alias scripts="gedit /home/aeneas/Projects/Scripts/startup.sh"
+
+# Converting Hashcat.bin to Hashcat
+alias hashcat="./hashcat.bin"
