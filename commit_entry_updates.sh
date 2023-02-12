7d5c338e16d538cad7d7fad475d6327355934905
ACTwo <streamcloud20@gmail.com>
2023-02-12 11:39:22 +0100

    backing up scripts

---
+++ commit_entry_updates.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cd /home/aeneas/Projects/ACsPySearch_Entries
+git add .
+git commit -m "Updating Personal Entries"
+git push
+
--- startup.sh
+++ startup.sh
@@ -33,10 +33,35 @@ alias sourcescripts="source /home/aeneas/Projects/Scripts/startup.sh"
 # cd to Hashcat
 alias cdhashcat="cd /home/aeneas/Projects/hashcat-6.2.5/"
 	
-
-
 # Edit this File
 alias scripts="gedit /home/aeneas/Projects/Scripts/startup.sh"
 
 # Converting Hashcat.bin to Hashcat
 alias hashcat="./hashcat.bin"
+
+#cd HashcatUI
+alias cdhashcatui="cd /home/aeneas/Projects/HashcatUI/"
+
+#cd ML-Folder
+alias cdml="cd /home/aeneas/Projects/ML/"
+
+#add pysearch_entries path
+export PATH="/home/aeneas/Projects/ACsPySearch_Entries:$PATH"
+
+#firefox as standardbrowser
+export BROWSER=firefox
+
+#add scripts folder to path
+export PATH="/home/aeneas/Projects/Scripts:$PATH"
+
+#export java
+export JAVA_HOME="/usr/lib/jvm/default-java"
+
+#export Python_Search entries
+export PYTHON_SEARCH_ENTRIES_FOLDER="/home/aeneas/Projects/ACsPySearch_Entries"
+
+#export Python_Search config
+export PYTHON_SEARCH_CONFIG_FOLDER="/home/aeneas/Projects/ACsPySearch_Entries"
+
+#cd into Projects/PythonSearch
+alias cdpythonsearch="cd /home/aeneas/Projects/PythonSearch/"
